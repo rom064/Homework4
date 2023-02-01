34f3c2b9347a7b9b47b775a3ca9a403305188aff
rom064 <rom064@mail.ru>
2023-02-01 19:23:53 +0500

    Сделано ДЗ

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+            task1();
+            task2();
+           // task3();
+          //  task4();
+          //  task5();
+          //  task6();
+          //  task7();
+          //  task8();
+        }
+
+        public static void task1() {
+            System.out.println("Задача 1");
+
+            System.out.println(" ");
+        }
+
+        public static void task2() {
+            System.out.println("Задача 2");
+
+            System.out.println(" ");
+        }
+
+
 }
